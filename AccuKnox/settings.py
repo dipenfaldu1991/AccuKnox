beb2f66d727b70f49fff1393435718d1df01bc8f
Dipen Faldu <42486085+dipenfaldu1991@users.noreply.github.com>
2023-12-11 14:00:33 +0530

    Update settings.py
    Database changes

--- AccuKnox/settings.py
+++ AccuKnox/settings.py
@@ -80,9 +80,13 @@ WSGI_APPLICATION = 'AccuKnox.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+     'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'accuknox',
+        'USER': 'admin',
+        'PASSWORD': 'admin',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
